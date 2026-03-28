4ffc9c8bc07cc58a87942739fee9bd88029f4507
patrycjawiecz <153092997+patrycjawiecz@users.noreply.github.com>
2026-03-28 15:17:27 +0100

    Updated progressive damage analysis for symmetric laminates with in plane loads
    Updated comments and changed plots

--- progressive_damage.py
+++ progressive_damage.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+### This file assumes symmetric laminate and only in plane loads,
+# so the calculations can be simplified
+
+
 #######################
 #   strain = S stress
 #   stress = C strain
@@ -37,6 +41,7 @@ def transformation_matrices(theta):
 
 
 def offaxis_matrices(Q, theta):
+    # Rotates Q matrix into local fibre orientations
     T_sigma, T_epsilon = transformation_matrices(theta)
     # S_theta = np.linalg.inv(T_epsilon) @ S @ T_sigma
     Q_theta = np.linalg.inv(T_sigma) @ Q @ T_epsilon
@@ -45,14 +50,15 @@ def offaxis_matrices(Q, theta):
 
 def calc_A(Q_all, t, n_plies):
     # Inputs:   Array consisting of Q for each ply in fibre orientations,
-    #           thickness of each ply (assumes the same for all),
+    #           thickness of each ply (assumes the same for all) in mm,
     #           number of plies
-    # Output:   A matrix, can be used instead of full ABD if only in plane loads are applied, reduces computation time
+    # Output:   A matrix, can be used instead of full ABD if only in plane loads are applied,
+    #           which reduces computation time
     A = np.zeros((3, 3))
-    T_midplane = t * n_plies / 2  # define where midplane is
+    t_midplane = t * n_plies / 2  # defines where midplane is
     for k in range(n_plies):
-        z_k = t * k - T_midplane
-        z_k_next = t * (k + 1) - T_midplane
+        z_k = t * k - t_midplane
+        z_k_next = t * (k + 1) - t_midplane
         A[:, :] += Q_all[:, :, k] * (z_k_next - z_k)
     return A
 
@@ -88,21 +94,21 @@ def lamina_strains_inplane(angles, abd, F, n_plies, active_plies):
     # converts the strain in 12 direction in midplane to strains in each ply IN LOCAL (PLY) COORDINATE SYSTEMS.
     # returns 3 x number of plies matrix of strains for all plies in local ply coordinates
     # Inputs: array with orientation angles of each ply
-    #         ABD matrix
-    #         6x1 force vector F
-    #         thickness of the plies
+    #         inverted A matrix
+    #         3x1 force intensity vector F (Nx, Ny, Ns) in N/mm
+    #         thickness of the plies (assumes the same for all)
     #         number of plies
+    # Outputs: 1 x 3 x n_plies array with 1, 2, and 12 strain values per ply
 
     strain_xy_mid = abd @ F
-    strain = np.zeros((1, 3, n_plies))  # 2 for bottom and top values; 3 for x, y, shear; per ply
-    # z_midplane = t * n_plies / 2
+    strain = np.zeros((1, 3, n_plies))  # 1 per ply (constant throughout the ply since only in-plane loads);
+    # 3 for 1, 2, and 12 strains; for the number of plies
+
     for k in range(n_plies):
         if active_plies[k] == True:
             T_sigma, T_epsilon = transformation_matrices(angles[k])
 
             strain_12_mid = T_epsilon @ strain_xy_mid
-            # z_k_bottom = t * k - z_midplane
-            # z_k_top = t * (k + 1) - z_midplane
 
             strain[0, :, k] = [strain_12_mid[0],
                                strain_12_mid[1],
@@ -112,13 +118,18 @@ def lamina_strains_inplane(angles, abd, F, n_plies, active_plies):
 
 def lamina_stress_inplane(strain, Q_0, Q_0degraded, n_plies, failed_once):
     # calculates stresses for each ply in local ply coordinates
-    # Inputs: Q_0 array per initial ply at 0 deg
+    # Inputs: 1 x 3 x n_plies array with 1, 2, and 12 strain values per ply
+    #         Q_0 array per initial ply at 0 deg
     #         Q_0degraded per degraded ply at 0 deg
+    #         n_plies number of plies
+    #         true/false array stating whether a ply is able to carry loads (by default all true)
+    # Outputs: 1x3xn_plies array with 1, 2, and 12 stress values for each ply
+
     stress = np.zeros((1, 3, n_plies))
+
     for i in range(n_plies):
         if failed_once[i] == False:
             stress[0, :, i] = Q_0 @ strain[0, :, i]
-
         else:
             stress[0, :, i] = Q_0degraded @ strain[0, :, i]
 
@@ -126,25 +137,54 @@ def lamina_stress_inplane(strain, Q_0, Q_0degraded, n_plies, failed_once):
 
 
 def failure(stress, strain, E1, Ef, v12f, Xt, Xc, Yt, Yc, Stc, n_plies, active_plies1, failed_once1):
-    ################# FIND Ef AND v12f !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    ##### find source for why snnL = 0.25 and snnT = 0.25
-    snnL = 0.25
-    snnT = 0.25
-    Stz = 144.5  # placeholder
-    R = Yc / (2 * (1 + snnT))
-    Sc = Stc * np.sqrt(1 + 2 * snnT)
+    # Puck failure criterion
+    # Inputs: 1x3xn_plies array with 1, 2, and 12 stress values for each ply (in MPa);
+    #         1x3xn_plies array with 1, 2, and 12 strain values per ply (unitless strain);
+    #         material properties with units:
+    #           E1, Ef, Xt, Xc, Yt, Yc, Stc - MPa
+    #           v12f - unitless
+    #         number of plies;
+    #         true/false array stating whether a ply is able to carry loads (by default all true);
+    #         true/false array stating whether a ply failed in transverse direction only
+    #                            (0.15 degradation rule is applied) ((by default all false))
+    #
+    # Output: updated true/false array stating whether a ply is able to carry loads;
+    #         updated true/false array stating whether a ply failed in transverse direction only
+    #         (0.15 degradation rule is applied)
+
+    # Puck failure parameters
+    p_parallel_t = 0.35  # [-]
+    p_parallel_c = 0.30  # [-]
+    p_perp_t = 0.25  # [-]
+    p_perp_c = 0.25  # [-]
+    Ytdegraded = 0.15 * Yt  # MPa
+    Ycdegraded = 0.15 * Yc  # MPa
+    Stz = np.sqrt(((1 + Yt / Yc) / (3 + 5 * Yt / Yc)) * Yt * Yc)  # MPa
+
     active_plies = active_plies1.copy()
     failed_once = failed_once1.copy()
+
     for i in range(n_plies):
         if active_plies[i] == True:
             a = 0
-
+            if failed_once1[i] == True:
+                Yt = Ytdegraded
+                Yc = Ycdegraded
             # Fibre tension or compression failure
             strainL = strain[0, 0, i]
             stressT = stress[0, 1, i]
             stressTL = stress[0, 2, i]
             eps = 1e-12
             stressTL = max(abs(stressTL), eps)
+
+            if stressT >= 0:
+                snnL = p_parallel_t
+                snnT = p_perp_t
+            else:
+                snnL = p_parallel_c
+                snnT = p_perp_c
+            R = Yc / (2 * (1 + snnT))
+            Sc = Stc * np.sqrt(1 + 2 * snnT)
             bracket = (strainL + v12f / Ef * 1.1 * stressT)
             if bracket >= 0:
                 X = Xt
@@ -187,48 +227,45 @@ def failure(stress, strain, E1, Ef, v12f, Xt, Xc, Yt, Yc, Stc, n_plies, active_p
                     else:
                         a += 1
 
-
             if a > 0:
                 failed_once[i] = True
 
     return active_plies, failed_once
 
-if __name__ == '__main__': #Only Run this code if this file is run (and not if it is being imported)
+
+if __name__ == '__main__':  # Only Run this code if this file is run (and not if it is being imported)
     # material data
     # UD lamina
     E1 = 172.3 * 10 ** 3  # MPa
     E2 = 10.2 * 10 ** 3  # MPa
     G12 = 5.58 * 10 ** 3  # MPa
     v12 = 0.25  # [-]
-    '''thickness is still a placeholder value'''
-    t = 0.125  # mm
+    t = 0.125  # mm  placeholder
     # Failure criteria
     Xt = 1923  # MPa
     Xc = 1480  # MPa
     Yt = 84  # MPa
     Yc = 220  # MPa
     Stc = 144.5  # MPa
-    Ef = E1
-    v12f = v12
-
+    Ef = 230 * 10 ** 3  # MPa Toray T300 fibres
+    v12f = 0.25
     base = [0, 45, -45, 90, 30]
     sym1 = (base) + (base)[::-1]
     sym2 = sym1 + sym1[::-1]
     sym3 = sym2 + sym2[::-1]
     angles = np.pi / 180 * np.array(base)
     n_plies = int(len(base))  # total number of plies
-
-
+    Stz = np.sqrt(((1 + Yt / Yc) / (3 + 5 * Yt / Yc)) * Yt * Yc)
+    print(Stz)
 
     S, Q_0 = inplane_matrices(E1, E2, v12, G12)
 
+    # Computed once at the beginning, so that it doesn't have to be done at every step
     Q_0degraded = Q_0.copy()
     Q_0degraded[1, 1] *= 0.15
     Q_0degraded[0, 1] *= 0.15
     Q_0degraded[1, 0] *= 0.15
     Q_0degraded[2, 2] *= 0.15
-
-
 
     number = 90
     ratios = np.zeros((2, number))
@@ -247,6 +284,10 @@ if __name__ == '__main__': #Only Run this code if this file is run (and not if i
     load_step = 1.0
     max_steps = 1000
 
+    lpfstrainsNyNs = np.zeros((3, number))
+    fpfstrainsNyNs = np.zeros((3, number))
+    lpfstrainsNxNy = np.zeros((3, number))
+    fpfstrainsNxNy = np.zeros((3, number))
     for i in range(number):
         # reset laminate state for this loading ratio
         active_plies = np.ones(n_plies, dtype=bool)
@@ -259,7 +300,7 @@ if __name__ == '__main__': #Only Run this code if this file is run (and not if i
         # loading direction for this ratio
         direction = np.array([ratios[0, i], ratios[1, i], 0.0])
 
-        Fxy = load_step * direction
+        Fxy = load_step * direction  # force intensity in N/mm
         Fxy_increment = load_step * direction
 
         fpf_recorded = False
@@ -293,6 +334,80 @@ if __name__ == '__main__': #Only Run this code if this file is run (and not if i
                 if not fpf_recorded:
                     first_ply_failure_NxNy[0, i] = Fxy[0]
                     first_ply_failure_NxNy[1, i] = Fxy[1]
+                    current_strain = abd @ Fxy
+                    fpfstrainsNxNy[:, i] = current_strain
+                    fpf_recorded = True
+
+                # update state
+                active_plies = active_new
+                failed_once = failed_new
+
+                # if all plies are gone, record LPF at this load
+                if not np.any(active_plies):
+                    last_ply_failure_NxNy[0, i] = Fxy[0]
+                    last_ply_failure_NxNy[1, i] = Fxy[1]
+                    current_strain = abd @ Fxy
+                    lpfstrainsNxNy[:, i] = current_strain
+                    break
+
+                # rebuild stiffness and analyze again at the SAME load
+                Q_all = calc_Q_laminate(Q_0, Q_0degraded, angles, active_plies, failed_once, n_plies)
+                ABD = calc_A(Q_all, t, n_plies)
+                abd = np.linalg.inv(ABD)
+
+                continue
+
+            # no damage at this load -> increase load
+            Fxy = Fxy + Fxy_increment
+
+    for i in range(number):
+        # reset laminate state for this loading ratio
+        active_plies = np.ones(n_plies, dtype=bool)
+        failed_once = np.zeros(n_plies, dtype=bool)
+
+        Q_all = calc_Q_laminate(Q_0, Q_0degraded, angles, active_plies, failed_once, n_plies)
+        ABD = calc_A(Q_all, t, n_plies)
+        abd = np.linalg.inv(ABD)
+
+        # loading direction for this ratio
+        direction = np.array([0.0, ratios[1, i], ratios[0, i]])
+
+        Fxy = load_step * direction
+        Fxy_increment = load_step * direction
+
+        fpf_recorded = False
+        step_counter = 0
+
+        while np.any(active_plies):
+            step_counter += 1
+            if step_counter > max_steps:
+                print(f"max_steps reached at ratio {i}")
+                break
+
+            # save old state explicitly
+            active_old = active_plies.copy()
+            failed_old = failed_once.copy()
+
+            strain = lamina_strains_inplane(angles, abd, Fxy, n_plies, active_plies)
+            stress = lamina_stress_inplane(strain, Q_0, Q_0degraded, n_plies, failed_once)
+
+            active_new, failed_new = failure(
+                stress, strain, E1, Ef, v12f, Xt, Xc, Yt, Yc, Stc,
+                n_plies, active_plies, failed_once
+            )
+
+            state_changed = (
+                    (not np.array_equal(active_new, active_old)) or
+                    (not np.array_equal(failed_new, failed_old))
+            )
+
+            if state_changed:
+                # first state change = first ply failure
+                if not fpf_recorded:
+                    first_ply_failure_NyNs[0, i] = Fxy[2]
+                    first_ply_failure_NyNs[1, i] = Fxy[1]
+                    current_strain = abd @ Fxy
+                    fpfstrainsNyNs[:, i] = current_strain
                     fpf_recorded = True
 
                 # update state
@@ -301,8 +416,10 @@ if __name__ == '__main__': #Only Run this code if this file is run (and not if i
 
                 # if all plies are gone, record LPF at this same load
                 if not np.any(active_plies):
-                    last_ply_failure_NxNy[0, i] = Fxy[0]
-                    last_ply_failure_NxNy[1, i] = Fxy[1]
+                    last_ply_failure_NyNs[0, i] = Fxy[2]
+                    last_ply_failure_NyNs[1, i] = Fxy[1]
+                    current_strain = abd @ Fxy
+                    lpfstrainsNyNs[:, i] = current_strain
                     break
 
                 # rebuild stiffness and re-analyze at SAME load
@@ -315,24 +432,71 @@ if __name__ == '__main__': #Only Run this code if this file is run (and not if i
             # no damage at this load -> increase load
             Fxy = Fxy + Fxy_increment
 
-    first_ply_failure_NxNy *= 2**3
-    last_ply_failure_NxNy *= 2**3
+    # The code was optimised strictly for the assignment task (multiple symmetries and only in plane loads)
+    # We calculated the FPF and LPF of 1/8 of the stack, and now we need to multiply it back times 8
+    last_ply_failure_NyNs *= 2 ** 3
+    first_ply_failure_NyNs *= 2 ** 3
+    first_ply_failure_NxNy *= 2 ** 3
+    last_ply_failure_NxNy *= 2 ** 3
 
-    print("FPF NxNy:")
+    '''print("FPF NxNy:")
     print(first_ply_failure_NxNy)
 
     print("LPF NxNy:")
-    print(last_ply_failure_NxNy)
+    print(last_ply_failure_NxNy)'''
+
+    # Close the curves
+    first_ply_failure_NxNy = np.hstack((first_ply_failure_NxNy, first_ply_failure_NxNy[:, [0]]))
+    last_ply_failure_NxNy = np.hstack((last_ply_failure_NxNy, last_ply_failure_NxNy[:, [0]]))
+    first_ply_failure_NyNs = np.hstack((first_ply_failure_NyNs, first_ply_failure_NyNs[:, [0]]))
+    last_ply_failure_NyNs = np.hstack((last_ply_failure_NyNs, last_ply_failure_NyNs[:, [0]]))
 
     plt.figure(figsize=(7, 7))
-    plt.scatter(first_ply_failure_NxNy[0, :], first_ply_failure_NxNy[1, :], label='FPF')
-    plt.scatter(last_ply_failure_NxNy[0, :], last_ply_failure_NxNy[1, :], label='LPF')
+    plt.plot(first_ply_failure_NxNy[0, :], first_ply_failure_NxNy[1, :], label='FPF')
+    plt.plot(last_ply_failure_NxNy[0, :], last_ply_failure_NxNy[1, :], label='LPF')
     plt.axhline(0, linewidth=0.8)
     plt.axvline(0, linewidth=0.8)
-    plt.xlabel('Nx')
-    plt.ylabel('Ny')
+    plt.xlabel('Nx [N/mm]')
+    plt.ylabel('Ny [N/mm]')
     plt.title('Failure Envelope (Nx-Ny)')
     plt.legend()
     plt.grid(True)
     plt.axis('equal')
     plt.show()
+
+    plt.figure(figsize=(7, 7))
+    plt.plot(first_ply_failure_NyNs[0, :], first_ply_failure_NyNs[1, :], label='FPF')
+    plt.plot(last_ply_failure_NyNs[0, :], last_ply_failure_NyNs[1, :], label='LPF')
+    plt.axhline(0, linewidth=0.8)
+    plt.axvline(0, linewidth=0.8)
+    plt.xlabel('Ns [N/mm]')
+    plt.ylabel('Ny [N/mm]')
+    plt.title('Failure Envelope (Ny-Ns)')
+    plt.legend()
+    plt.grid(True)
+    plt.axis('equal')
+    plt.show()
+
+    # stack strains to make a nice txt file i can import into excel
+    dataxy = np.hstack((
+        fpfstrainsNxNy.T,
+        lpfstrainsNxNy.T
+    ))
+    datays = np.hstack((
+        fpfstrainsNyNs.T,
+        lpfstrainsNyNs.T,
+    ))
+
+    # Create header (12 columns total: 4 arrays × 3 components)
+    headerxy = (
+        "fpf_NxNy_1,fpf_NxNy_2,fpf_NxNy_3"
+        "lpf_NxNy_1,lpf_NxNy_2,lpf_NxNy_3,"
+    )
+    headerys = (
+        "fpf_NyNs_1,fpf_NyNs_2,fpf_NyNs_3,"
+        "lpf_NyNs_1,lpf_NyNs_2,lpf_NyNs_3,"
+    )
+
+    # Save to file
+    np.savetxt("strainsxy.txt", dataxy, delimiter=",", header=headerxy, comments="")
+    np.savetxt("strainsys.txt", datays, delimiter=",", header=headerys, comments="")
